61cac57c9b3115f09a15fa8816f93c2d582d8574
Artem <jaman.smlnsk@gmail.com>
2020-05-03 08:08:01 +0300

    2020-05-03 Version 2.3.0: Updated tests

--- app/src/test/java/com/smlnskgmail/jaman/cryptotracker/TestSuite.kt
+++ app/src/test/java/com/smlnskgmail/jaman/cryptotracker/TestSuite.kt
@@ -1,10 +1,15 @@
 package com.smlnskgmail.jaman.cryptotracker
 
+import com.smlnskgmail.jaman.cryptotracker.model.CurrencyPricePercentChangeDayTest
+import com.smlnskgmail.jaman.cryptotracker.model.CurrencyPriceTest
+import com.smlnskgmail.jaman.cryptotracker.model.CurrencyTypeTest
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
 @Suite.SuiteClasses(
-
+    CurrencyPriceTest::class,
+    CurrencyPricePercentChangeDayTest::class,
+    CurrencyTypeTest::class
 )
 @RunWith(Suite::class)
 class TestSuite
